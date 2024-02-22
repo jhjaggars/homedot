6e4453d36ec45f1b29c0c002b6e7cfd719f34cac
Jesse Jaggars <jhjaggars@gmail.com>
2024-02-21 19:53:15 -0500

    adding back harpoon mappings

--- nvim-config/init.lua
+++ nvim-config/init.lua
@@ -75,19 +75,19 @@ require('lazy').setup({
   },
   {
     'Mofiqul/dracula.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'dracula'
-    end,
+    -- priority = 1000,
+    -- config = function()
+    --   vim.cmd.colorscheme 'dracula'
+    -- end,
   },
 
   {
     'catppuccin/nvim',
     name = 'catppuccin',
-    -- priority = 1000,
-    -- config = function()
-    --   vim.cmd.colorscheme 'catppuccin-mocha'
-    -- end,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'catppuccin-mocha'
+    end,
   },
   {
     'stevearc/oil.nvim',
@@ -216,6 +216,9 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+
+vim.keymap.set('n', '<leader>ha', require('harpoon.mark').add_file, { desc = 'Add file to Harpoon' })
+vim.keymap.set('n', '<leader>hh', require('harpoon.ui').toggle_quick_menu, { desc = 'Open Harpoon' })
 
 -- [[ Configure Coverage ]] --
 vim.keymap.set('n', '<leader>cc', function()
